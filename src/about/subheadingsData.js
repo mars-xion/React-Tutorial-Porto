b75a2527f9d46ac4bbe02f2ce62c35e101fac514
mars-xion <maz200027@gmail.com>
2024-01-13 13:42:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,76 @@
+const subheadingsData = {
+  // Personal
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+
+  // Education
+  2: [
+    {
+      title: "2018 d degree",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+
+  3: [
+    {
+      title: "2024 uni degree",
+      content: (
+        <>
+          <p>
+            Lorema lkfkjsldajf sjfasjdf dsfjsdlfjsd flsjdf sdfljflkdjdflj
+            dalskfjdsjf lsjdfdlkasfd .
+          </p>
+        </>
+      ),
+    },
+  ],
+
+  4: [
+    {
+      title: "2023 Minor in Turkey",
+      content: (
+        <>
+          <p>
+            Lorema lkfkjsldajf sjfasjdf dsfjsdlfjsd flsjdf sdfljflkdjdflj
+            dalskfjdsjf lsjdfdlkasfd .
+          </p>
+        </>
+      ),
+    },
+  ],
+
+  //career
+  5: [
+    {
+      title: "2022 Interinship in MOH, Oman",
+      content: (
+        <>
+          <p>
+            Lorema lkfkjsldajf sjfasjdf dsfjsdlfjsd flsjdf sdfljflkdjdflj
+            dalskfjdsjf lsjdfdlkasfd .
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
